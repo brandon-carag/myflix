036cccf0f1799c331f2fc3505672bedabc555509
Brandon Carag <brandon.carag@gmail.com>
2014-09-04 21:52:22 -0700

    cosmetic formatting of rspec tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,19 +3,20 @@ require 'shoulda/matchers'
 
 describe Category do
   
-#standard tests
-  it "saves itself" do
-    category=Category.new(name:"Test Category")
-    category.save
-    expect(Category.first).to eq(category)
-  end
+#unnecessary tests covered by rails or shoulda_matchers from thoughtbot
+  # it "saves itself" do
+  #   category=Category.new(name:"Test Category")
+  #   category.save
+  #   expect(Category.first).to eq(category)
+  # end
 
-  it "has_many videos" do
-    category=Category.create(name:"Test Category")
-    video=Video.create(title:"Futurama",description:"Test Descrip",small_cover_url:"/tmp/futurama.jpg",large_cover_url:"/tmp/futurama.jpg",category: category)
-    video2=Video.create(title:"Family Guy",description:"Test Descrip2",small_cover_url:"/tmp/family_guy.jpg",large_cover_url:"/tmp/family_guy.jpg",category:category)
-    expect(category.videos).to include(video,video2)
-  end
+  # it "has_many videos" do
+  #   category=Category.create(name:"Test Category")
+  #   video=Video.create(title:"Futurama",description:"Test Descrip",small_cover_url:"/tmp/futurama.jpg",large_cover_url:"/tmp/futurama.jpg",category: category)
+  #   video2=Video.create(title:"Family Guy",description:"Test Descrip2",small_cover_url:"/tmp/family_guy.jpg",large_cover_url:"/tmp/family_guy.jpg",category:category)
+  #   expect(category.videos).to include(video,video2)
+  # end
+
 
 #shoulda_matchers_from_thoughtbot
   it { should have_many(:videos) }
