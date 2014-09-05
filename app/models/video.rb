a7906094554ec0377e1f05bfb1b6d4d03848d1c7
Brandon Carag <brandon.carag@gmail.com>
2014-09-04 21:47:56 -0700

    Implemented search_by_title spec

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,15 @@
 class Video < ActiveRecord::Base;
   belongs_to :category
   validates_presence_of :title,:description
+
+  def self.search_by_title(query_string)
+    if query_string==""
+      []
+    else
+      Video.where("title like?","%#{query_string}%")
+      # Video.where("title LIKE :variable, variable: "#{query_string}")
+    end
+  end
+
+
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'shoulda/matchers'
+require 'pry'
 
 describe Video do
 
@@ -28,12 +29,39 @@ describe Video do
     expect(Video.all.size).to eq(0) || expect(video.errors.any?)==true
   end
 
+#Active Record search_by_title method
+  it ".search_by_title returns nothing for empty string" do
+    futurama=Video.create(title:"Futurama",description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999. -IMDB",small_cover_url:"/tmp/futurama.jpg",large_cover_url:"/tmp/futurama.jpg",category_id:1)
+    expect(Video.search_by_title("")).to eq([])
+  end
+
+  it ".search_by_title finds a single record" do
+    futurama=Video.create(title:"Futurama",description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999. -IMDB",small_cover_url:"/tmp/futurama.jpg",large_cover_url:"/tmp/futurama.jpg",category_id:1)
+    family_guy=Video.create(title:"Family Guy",description:"Family Guy is an American adult animated sitcom created by Seth MacFarlane for the Fox Broadcasting Company. The series centers on the Griffins, a family consisting of parents Peter and Lois; their children Meg, Chris, and Stewie; and their anthropomorphic pet dog Brian. The show is set in the fictional city of Quahog, Rhode Island, and exhibits much of its humor in the form of cutaway gags that often lampoon American culture. -Wikipedia",small_cover_url:"/tmp/family_guy.jpg",large_cover_url:"/tmp/family_guy.jpg",category_id:1)
+    monk=Video.create(title:"Monk",description:"The exploits of a detective with a unique case of obsessive compulsive disorder",small_cover_url:"/tmp/monk.jpg",large_cover_url:"/tmp/monk_large.jpg",category_id:2)
+    expect(Video.search_by_title("Monk")).to include(monk)
+  end
+
+  it ".search_by_title finds multiple records" do
+    futurama=Video.create(title:"Futurama",description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999. -IMDB",small_cover_url:"/tmp/futurama.jpg",large_cover_url:"/tmp/futurama.jpg",category_id:1)
+    futurama2=Video.create(title:"My title has Futurama in it",description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999. -IMDB",small_cover_url:"/tmp/futurama.jpg",large_cover_url:"/tmp/futurama.jpg",category_id:1)
+    monk=Video.create(title:"Monk",description:"The exploits of a detective with a unique case of obsessive compulsive disorder",small_cover_url:"/tmp/monk.jpg",large_cover_url:"/tmp/monk_large.jpg",category_id:2)
+    expect(Video.search_by_title("Futurama")).to include(futurama,futurama2)
+  end
+
+  # it ".search_by_title finds a single record" do
+  #   futurama=Video.create(title:"Futurama",description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999. -IMDB",small_cover_url:"/tmp/futurama.jpg",large_cover_url:"/tmp/futurama.jpg",category_id:1)
+  #   family_guy=Video.create(title:"Family Guy",description:"Family Guy is an American adult animated sitcom created by Seth MacFarlane for the Fox Broadcasting Company. The series centers on the Griffins, a family consisting of parents Peter and Lois; their children Meg, Chris, and Stewie; and their anthropomorphic pet dog Brian. The show is set in the fictional city of Quahog, Rhode Island, and exhibits much of its humor in the form of cutaway gags that often lampoon American culture. -Wikipedia",small_cover_url:"/tmp/family_guy.jpg",large_cover_url:"/tmp/family_guy.jpg",category_id:1)
+  #   monk=Video.create(title:"Monk",description:"The exploits of a detective with a unique case of obsessive compulsive disorder",small_cover_url:"/tmp/monk.jpg",large_cover_url:"/tmp/monk_large.jpg",category_id:2)
+  #   expect(Video.search_by_title("Monk")).to include([monk])
+  # end
+
+  
+
 #shoulda_matchers_from_thoughtbot
   it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
-
-  
 
 end
