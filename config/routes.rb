201ae119e10ec7dd8ce7e5bd37d6bbf2d6ebf0ed
Brandon Carag <brandon.carag@gmail.com>
2014-09-08 10:52:49 -0700

    Added register path,bcrypt,bootstrap_form_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Myflix::Application.routes.draw do
   
   root to: 'videos#index'
+  get '/register', to: 'users#new'
   get '/home', to: 'videos#index'
   # post '/search', to: 'videos#search'
   get 'ui(/:action)', controller: 'ui'
 
+  resources :users, only: [:show,:index,:create]
   resources :videos, except: :destroy do
     collection do
       get '/search', to: 'videos#search'
